ced8ca923a8fa2278918365310f594abda9059d8
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-02-15 18:45:55 -0500

    views de log/signup y rutas

--- woosher-ironhackProject2/app.js
+++ woosher-ironhackProject2/app.js
@@ -12,7 +12,7 @@ const passport = require('./helpers/passport')
 const session = require('express-session')
 
 mongoose
-  .connect('mongodb://localhost/woosher-ironhackproject2', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -76,7 +76,9 @@ function isLogged(req, res, next) {
 //
 
 const index = require('./routes/index');
-app.use('/', index);
+const auth = require('./routes/auth')
+app.use('/', isLogged, auth)
+app.use('/', isLogged, index);
 
 
 module.exports = app;
