7aba92569f0537b8d12476e203851329422ba019
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-02-15 16:21:24 -0500

    primer commit

---
+++ woosher-ironhackProject2/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const PLM = require("passport-local-mongoose")
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    lastName:{
+      type: String,
+      required: true
+    },
+    address: {
+      type:String,
+      required: true,
+    },
+    email: String,
+
+    isWorker:{
+      type: Boolean,
+      default: null
+    },
+    photoURL: {
+      type: String,
+      default:
+        '',
+    },
+
+  } , {timestamps:true}
+)
+
+userSchema.plugin(PLM, { usernameField: 'email' })
+
+module.exports = mongoose.model("User", userSchema)
