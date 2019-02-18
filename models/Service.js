75fa2ed05ece96eaed86c8750c5e4ea6b22fc083
Isabella Onate <isabellaonate@gmail.com>
2019-02-18 12:54:20 -0600

    Changing info and verifying commas within models. Added the review model.

--- models/Service.js
+++ models/Service.js
@@ -1,41 +1,46 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const Schema = require("mongoose").Schema
 
-const serviceSchema = new Schema(
-    {
-        user:{
-                type: Schema.Types.ObjectId,
-                ref: "User"
-            },
-        
-        worker:{
-            type: Schema.Types.ObjectId,
-            ref:"User"
+const serviceSchema = new Schema({
+
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+
+      worker: {
+        type: Schema.Types.ObjectId,
+        ref:"User",
+      },
+
+      active: {
+        type: Boolean,
+        default: false,
+      },
+
+      addressUser: {
+        location: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
         },
-        active:{
-            type: Boolean,
-            default: false
+        required: true,
+        active: true,
+        coordinates: [],
+      },
+
+      addressWoosher: {
+        location: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
         },
-        addressFrom: {
-            location: {
-              type: Schema.Types.ObjectId,
-              ref: 'User',
-            },
-            coordinates: []
-          },
-          addressFrom:{
-            location: {
-                type: Schema.Types.ObjectId,
-                ref: 'User',
-              },
-              coordinatesTo: []     
-          },
-        requestDate:{
-            type: Date,
-            required: true
-        }
-        
-    } , {timestamps: true}
-)
+        active: false,
+        coordinatesTo: [],
+      },
+
+      requestDate: {
+        type: Date,
+        required: true,
+      },
+
+},{timestamps:true})
 
 module.exports = mongoose.model("Service", serviceSchema)
--- models/User.js
+++ models/User.js
@@ -1,33 +1,32 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const Schema = require("mongoose").Schema
 const PLM = require("passport-local-mongoose")
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true
-    },
-    lastName:{
-      type: String,
-      required: true
-    },
-    address:String,
-    email: String,
+const userSchema = new Schema({
 
-    isWorker:{
-      type: Boolean,
-      default: false,
-    },
-    photoURL: {
-      type: String,
-      default:
-        'https://res.cloudinary.com/dpt8pbi8n/image/upload/v1550267245/kisspng-user-interface-design-computer-icons-default-stephen-salazar-photography-5b1462e1b19d70.1261504615280626897275.jpg',
-    },
+  name: {
+    type: String,
+    required: true,
+  },
 
-  } , {timestamps:true}
-)
+  lastName:{
+    type: String,
+    required: true,
+  },
 
-userSchema.plugin(PLM, { usernameField: 'email' })
+  address: String,
+  email: String,
 
+  isWoosher:{
+    type: Boolean,
+    default: false,
+  },
+
+  photoURL: {
+    type: String,
+    default: 'images/user',
+  },
+
+},{timestamps:true})
+
+userSchema.plugin(PLM, {usernameField: 'email'})
 module.exports = mongoose.model("User", userSchema)
